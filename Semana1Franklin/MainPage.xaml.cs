f02d5ceb5305e1b13d929c3057d21035c1739c38
SOLANGE <SOLANGE@DESKTOP-QMC5VH4>
2022-04-28 18:54:19 -0500

    Agregar archivos de proyecto.

---
+++ Semana1Franklin/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Semana1Franklin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEviar_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string edad = txtEdad.Text;
+
+            DisplayAlert("mensaje de alerta", "BIENVENIDO " + nombre + "  " + apellido + "  " + " edad " + edad, "cerrar");
+
+
+
+        }
+    }
+}
